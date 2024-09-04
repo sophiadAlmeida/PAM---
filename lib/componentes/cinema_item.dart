83ee87d276506736da2c08b22f28ec29624c49d5
unknown <sophia.almeida3@etec.sp.gov.br>
2024-09-03 22:36:27 -0300

    branch com o código da aula do dia Tue, Sep  3, 2024 10:36:27 PM

---
+++ lib/componentes/cinema_item.dart
@@ -0,0 +1,69 @@
+import 'package:cinemeu/entidade/cinema.dart';
+import 'package:flutter/material.dart';
+
+class CinemaItem extends StatelessWidget {
+  final Cinema cinema;
+
+  const CinemaItem({
+    super.key,
+    required this.cinema,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final tema = Theme.of(context);
+
+    return Container(
+      margin: const EdgeInsets.all(5),
+      child: Material(
+        elevation: 5,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.theaters,
+              size: 48,
+              color: Color.fromARGB(255, 100, 100, 100),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.only(left: 8.0, right: 12),
+                constraints: const BoxConstraints(
+                  minHeight: 25,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      cinema.nome,
+                      style: tema.textTheme.titleMedium?.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      cinema.endereco,
+                      style: tema.textTheme.bodySmall,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          cinema.telefone,
+                          style: tema.textTheme.bodySmall,
+                        ),
+                        Text(
+                          "Salas: ${cinema.salas}",
+                          style: tema.textTheme.titleSmall,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
